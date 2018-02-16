22b85af22b3c03d0394d9ede07d3380880d26b23
M-Aranda <cheloz_20@hotmail.com>
2018-02-16 14:31:43 -0300

    agregada hora y fechas de vencimiento, pero sin ajustar ninguna todavia, deje comentada la otra parte de tiempo de 5 en 5 minutos porque las 2 a la vez no corrian

--- src/rotulador/upa/Menu.java
+++ src/rotulador/upa/Menu.java
@@ -10,8 +10,8 @@ public class Menu {
     
     
     public void mostrarMenuDeInicio(){
-        System.out.println("Bienvenido al menu de creacion de rotulos Upa,");
-        System.out.println("por favor elija opcion:");
+        
+        System.out.println("Elija opcion:");
         
         System.out.println("1. Crear rotulos para food");
         System.out.println("2. Crear rotulos para cafe");
--- src/rotulador/upa/RotuladorUPA.java
+++ src/rotulador/upa/RotuladorUPA.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
@@ -15,8 +16,8 @@ public class RotuladorUPA {
 
     public static void main(String[] args) {
 
+        System.out.println("Bienvenido al menu de creacion de rotulos Upa,");
         Menu m = new Menu();
-        m.mostrarMenuDeInicio();
 
         Scanner s = new Scanner(System.in);
 
@@ -24,6 +25,7 @@ public class RotuladorUPA {
 
         while (true) {
             try {
+                m.mostrarMenuDeInicio();
                 System.out.print("Opcion: ");
                 op = Integer.parseInt(s.nextLine());
                 if (op == 4) {
@@ -38,17 +40,46 @@ public class RotuladorUPA {
 
 //                    System.out.print("Ingrese fecha de elaboracion: ");
 //                    String fE = s.nextLine();
-                    int hEN;
+                    int ele;
+
+                    System.out.println("Usar hora actual?");
+                    while (true) {
+                        try {
+
+                            System.out.println("1. Si");
+                            System.out.println("2. No");
+
+                            ele = Integer.parseInt(s.nextLine());
+                            if (ele == 1 || ele == 2) {
+                                break;
+                            } else if (ele != 1 && ele != 2) {
+                                System.out.println("Debe elegir 1 o 2");
+                            }
+
+                        } catch (Exception e) {
+                            System.out.println("Eso no es valido.");
+                        }
+                    }
+
+                    int hEN = 0;
+
                     while (true) {
 
                         try {
-                            System.out.print("Ingrese hora de elaboracion: ");
-                            hEN = Integer.parseInt(s.nextLine());
+                            if (ele == 2) {
+                                System.out.print("Ingrese hora de elaboracion: ");
+                                hEN = Integer.parseInt(s.nextLine());
 
-                            if (hEN <= 2359 && hEN >= 0000) {
+                                if (hEN <= 2359 && hEN >= 0000) {
+                                    System.out.println("Generacion manual de hora, seleccionada. Habra una diferencia de 5 minutos entre rotulo.");
+                                    break;
+                                } else {
+                                    System.out.println("El numero debe ser igual or mayor que 0000 y menor a 2400");
+                                }
+
+                            } else if (ele == 1) {
+                                System.out.println("Se generaran rotulos con una diferencia de 5 minutos entre si a partir de la hora de la creacion.");
                                 break;
-                            } else {
-                                System.out.println("El numero debe ser igual or mayor que 0000 y menor a 2400");
                             }
 
                         } catch (Exception e) {
@@ -70,30 +101,49 @@ public class RotuladorUPA {
                     while (true) {
                         IngredientedeFood ing = new IngredientedeFood();
                         ing.setFechaDeElaboracion(fE);
-                        String hEpf = Integer.toString(hEN);
-
-                        StringBuilder hEF = new StringBuilder(hEpf);
-                        if (hEN < 1000) {
-                            hEF.insert(1, ":");
-                            if (Integer.parseInt(hEpf.substring(1)) == 55) {
-                                hEN += 40;
-                            }
-                        } else if (hEN >= 1000) {
-                            hEF.insert(2, ":");
-                            if (Integer.parseInt(hEpf.substring(2)) == 55) {
-                                hEN += 40;
-                            }
-                        }
-
-                        ing.setHoraDeElaboracion(hEF.toString());
+//
+//                        String hEpf = Integer.toString(hEN);
+//
+//                        StringBuilder hEF = new StringBuilder(hEpf);
+//                        if (hEN < 1000) {
+//                            hEF.insert(1, ":");
+//                            if (Integer.parseInt(hEpf.substring(1)) == 55) {
+//                                hEN += 40;
+//                            }
+//                        } else if (hEN >= 1000) {
+//                            hEF.insert(2, ":");
+//                            if (Integer.parseInt(hEpf.substring(2)) == 55) {
+//                                hEN += 40;
+//                            }
+//                        }
+//
+//                        ing.setHoraDeElaboracion(hEF.toString());
 
                         Calendar calendar = Calendar.getInstance();//Crea nuevo calendario?
-//                        System.out.println(calendar.getTime()); print today's date
+//                        System.out.println(calendar.getTime()); muestra fecha de hoy
                         calendar.add(Calendar.DATE, 2);//agrega 2 dia a esa fecha del calendario
                         Date ddd = calendar.getTime();//guarda datos en variable
                         DateFormat df = new SimpleDateFormat("dd/MM/yyyy ");//se usa para darle formato String a la fecha
                         String fVM = df.format(ddd);//pasa la fecha a String
                         ing.setFechaDeVencimiento(fVM);//le da la fecha nueva como String para el metodo que la setea
+
+                        
+                        
+                        Calendar cale = Calendar.getInstance();
+                        Date hor = cale.getTime();
+                        int minutosAAgregar = 5;
+                        if (ele == 1) {
+                            for (int i = 0; i < 21; i++) {
+                                
+                                cale.add(Calendar.MINUTE, minutosAAgregar);
+
+                                DateFormat formatoTiempo = new SimpleDateFormat("HH:mm:ss");
+                                String horaEla = formatoTiempo.format(hor);
+                                ing.setHoraDeElaboracion(horaEla);
+                                minutosAAgregar += 5;
+                            }
+
+                        }
 
                         ing.setResponsable(res);
                         ingredientesF.add(ing);
@@ -106,8 +156,13 @@ public class RotuladorUPA {
                         }
 
                     }
+                    Calendar horaPrimerIngre = Calendar.getInstance();
+                    Date horaTomate = horaPrimerIngre.getTime();
+                    DateFormat formatoTomate = new SimpleDateFormat("HH:mm:ss");
+                    String horaTom = formatoTomate.format(horaTomate);
 
                     ingredientesF.get(0).setNombre("Tomate");
+                    ingredientesF.get(0).setHoraDeElaboracion(horaTom);
                     ingredientesF.get(1).setNombre("Salsa");
                     ingredientesF.get(2).setNombre("Queso");
                     ingredientesF.get(3).setNombre("Queso");
